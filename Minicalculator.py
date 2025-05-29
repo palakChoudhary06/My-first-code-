f49e9f090a312369a441ef90a8e90ad457da01be
palakChoudhary06 <jatr0098@gmail.com>
2025-05-29 10:53:54 +0530

    Create Minicalculator.py
    My first python code

---
+++ Minicalculator.py
@@ -0,0 +1,21 @@
+print("Hello World Python pyDroid")
+x= input("enter x number")
+operator = input("enter operator (+,-,*,/,//,**,%)":)
+y= input("enter your y number")
+
+if operator == "+":
+  print(x+y)
+elif operator== "-":
+  print(x-y)
+elif operator== "*":
+  print(x*y)
+elif operator=="/":
+  print(x/y)
+elif operator=="**":
+  print(x**y)
+elif operator=="//":
+  print(x//y)
+elif operator=="%":
+  print(x%y)
+else: 
+print("invalid operator")
